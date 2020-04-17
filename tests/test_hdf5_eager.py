8aca64ee9c4e82b8ce783559669db3ee82650e79
Yong Tang <yong.tang.github@outlook.com>
2020-04-17 16:19:09 -0700

    Work in Progress
    Signed-off-by: Yong Tang <yong.tang.github@outlook.com>

--- tests/test_hdf5_eager.py
+++ tests/test_hdf5_eager.py
@@ -25,38 +25,24 @@ import tensorflow as tf
 import tensorflow_io as tfio
 
 
-def test_hdf5():
-    """test_hdf5: GitHub issue 841"""
-
-    def create_datasets(runpath, cnt=10):
-        os.makedirs(runpath, exist_ok=True)
-        for i in range(cnt):
-            f = h5py.File("{}/file_{}.h5".format(runpath, i), "w")
-            total_samples = np.random.randint(50000, 100000)
-            f.create_dataset("features", data=np.random.random((total_samples, 60)))
-            f.create_dataset("targets", data=np.random.random((total_samples, 3)))
-            f.close()
+def test_hdf5_io_dataset_resource_reuse():
+    """test_hdf5_io_dataset_resource_reuse: GitHub issue 841"""
 
     runpath = tempfile.mkdtemp()
-    create_datasets(runpath)
-
-    for i in range(2):
-        cnt = 0
-        for p in glob.glob("{}/*.h5".format(runpath)):
-            try:
-                features = tfio.IODataset.from_hdf5(p, "/features")
-                targets = tfio.IODataset.from_hdf5(p, "/targets")
-                dataset = tf.data.Dataset.zip((features, targets))
-
-                for t in dataset:
-                    cnt += t[0].shape[0]
-
-            except Exception as e:
-                print("Failed going through {}".format(p))
-                raise e
-            print("Success going through {}".format(p))
-
-    print("Iterated {} items".format(cnt))
+    os.makedirs(runpath, exist_ok=True)
+    shapes = [(1, 2048), (1, 4096)]
+    for i, shape in enumerate(shapes):
+        filename = "{}/file_{}.h5".format(runpath, i)
+        f = h5py.File(filename, "w")
+        f.create_dataset("features", data=np.random.random(shape))
+        f.close()
+    filenames = ["{}/file_{}.h5".format(runpath, i) for i, shape in enumerate(shapes)]
+    dataset = tf.data.Dataset.from_tensor_slices(filenames)
+    #dataset = dataset.map(lambda e: tfio.IOTensor.from_hdf5(e, {"/features": tf.TensorSpec([None], tf.float64)})("/features").to_tensor())
+    dataset = dataset.map(lambda e: tfio.IODataset.from_hdf5(e, "/features", tf.float64))
+    dataset = dataset.flat_map(lambda e: e.map(tf.shape))
+    value = [e for e in dataset]
+    print("VALUE: ", value) #assert list(value) == list(shape[1:])
 
     shutil.rmtree(runpath)
 
